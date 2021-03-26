b1346c3e0c0696c354835cab5cd938dcbbebad15
Ricardo Filho <ricardinhogiasson16@gmail.com>
2021-03-26 10:40:04 -0300

    Corrigindo Bug

--- db_credentials.php
+++ db_credentials.php
@@ -1,16 +1,16 @@
 <?php
 
 
-//$servername = "gitlab.tadsufpr.net.br";
-//$username = "web1";
-//$password = "tads";
-//$dbname = "ds122_2021_especial_2";
+$servername = "gitlab.tadsufpr.net.br";
+$username = "web1";
+$password = "tads";
+$dbname = "ds122_2021_especial_2";
 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "MyCar";
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "MyCar";
 
 $table1 = "grr20204465_tb_produtos";
 $table2 = "grr20204465_tb_clientes";
